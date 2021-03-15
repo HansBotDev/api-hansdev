17d5ea30083c587c6b71dba03994d2af4cf7b25d
HansBotDev <80545133+HansBotDev@users.noreply.github.com>
2021-03-15 23:40:04 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,7 @@ var zahirr = db.get("hans");
 	console.log('')
 }
 
-var creatorList = ['@hansdev'];
+var creatorList = ['@mszpros'];
 var creator = creatorList[Math.floor(Math.random() * creatorList.length)];
 
 
